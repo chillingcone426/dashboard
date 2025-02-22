709b7043de991c07fb6454a3465b3f247d7f7fd5
chillingcone426 <89105733+chillingcone426@users.noreply.github.com>
2025-02-22 15:54:24 -0500

    Update constants.js

--- frontend/src/js/constants.js
+++ frontend/src/js/constants.js
@@ -1,7 +1,7 @@
-export const API_URL = env.API_URL || "https://ticketsapi2.rankblox.xyz/"
+export const API_URL = "https://ticketsapi2.rankblox.xyz/"
 export const PLACEHOLDER_DOCS_URL = "https://docs.ticketsbot.net/setup/placeholders.html"
 
 export const OAUTH = {
     clientId: env.CLIENT_ID || "1328078910168109106",
-    redirectUri: env.REDIRECT_URI || "https://tickets.rankblox.xyz/callback "
+    redirectUri: "https://tickets.rankblox.xyz/callback "
 }
